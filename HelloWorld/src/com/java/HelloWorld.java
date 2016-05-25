dcf1ed3481711783007350b935e35e5cfbe3eceb
zilulu <1256662936@qq.com>
2016-05-25 15:56:34 +0800

    modify dev

--- HelloWorld/src/com/java/HelloWorld.java
+++ HelloWorld/src/com/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("This is branch dev.");
+		System.out.println("try again.");
 	}
 
 }
