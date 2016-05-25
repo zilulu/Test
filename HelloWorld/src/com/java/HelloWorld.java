9617d5d4673361603dd8cec96619abe47af00557
zilulu <1256662936@qq.com>
2016-05-25 15:42:02 +0800

    # WARNING: head commit changed in the meantime
    Modify HelloWorld.java

--- HelloWorld/src/com/java/HelloWorld.java
+++ HelloWorld/src/com/java/HelloWorld.java
@@ -3,7 +3,8 @@ package com.java;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("hello world!");
+		System.out.println("This is branch dev.");
 	}
 
 }
